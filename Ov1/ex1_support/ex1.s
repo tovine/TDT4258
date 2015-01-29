0535db10322b49a4c4a92bce4a2fb76275dfb129
Marjeris Romero <marjeris.romero@gmail.com>
2015-01-29 13:30:30 +0100

    satt drive strength til 20mA (high) og pin 8-15 i port A (LEDs) som output, og high :)

--- Ov1/ex1_support/ex1.s
+++ Ov1/ex1_support/ex1.s
@@ -1,6 +1,3 @@
-        .syntax unified
-	
-	      .include "efm32gg.s"
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
@@ -82,9 +79,14 @@
 	      .globl  _reset
 	      .type   _reset, %function
         .thumb_func
-_reset: 
+_reset:
+	//Set high drive strength 
+	ldr r1, =GPIO_PA_BASE
+	mov r2, #0x2
+	str r2, [r1, #GPIO_CTRL]
+ 
 	//load CMU base address
-	ldr r1, CMU_BASE
+	ldr r1, =CMU_BASE
 	mov r2, #1				// Load '1' to temp. register r2
 	lsl r2, r2, #CMU_HFPERCLKEN0_GPIO	// Shift '1' to GPIO position
 	str r2, [r1, #CMU_HFPRCLKEN0]		// Write result to control register
@@ -92,14 +94,17 @@ _reset:
 	// Set up GPIO - PORT_C[0-7] = SWITCHES, PORT_A[8-15] = LEDS
 	// -> SWITCHES: strong pull-up, input (maybe filter) - MODEn = 0b0010, DOUT = 1
 	// -> LEDS: output mode
-	ldr r1, GPIO_PC_BASE
+	ldr r1,=GPIO_PC_BASE
 	mov r2, #0x22222222 // = 0b0010
 	str r2, [r1, #GPIO_MODEL]
 	
 	mov r2, #0xFF
 	str r2, [r1, #GPIO_DOUT]
 	
-
+	//LEDS, modeh=0b0100 push pull
+	ldr r1,=GPIO_PA_BASE
+	mov r2, #0x55555555
+	str r2, [r1, #GPIO_MODEH]
 	
 	/////////////////////////////////////////////////////////////////////////////
 	//
@@ -111,8 +116,11 @@ _reset:
         .thumb_func
 gpio_handler:  
 
-	      b .  // do nothing
-	
+//	      b .  // do nothing
+//Set LEDS high
+	ldr r1, =GPIO_PA_BASE
+	mov r2, #0xFF00  //set pin 15-8 high
+	orr r2, [r1, #GPIO_DOUT] 	
 	/////////////////////////////////////////////////////////////////////////////
 	
         .thumb_func
