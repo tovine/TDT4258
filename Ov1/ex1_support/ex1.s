90cd9f6a31b39c0fbc7adf4cab66e949e0d163c0
Marjeris Romero <marjeris.romero@gmail.com>
2015-02-03 11:16:28 +0100

    To leds

--- Ov1/ex1_support/ex1.s
+++ Ov1/ex1_support/ex1.s
@@ -1,4 +1,4 @@
-
+.include "efm32gg.s"
 	/////////////////////////////////////////////////////////////////////////////
 	//
   // Exception vector table
@@ -83,27 +83,28 @@ _reset:
 	//Set high drive strength 
 	ldr r1, =GPIO_PA_BASE
 	mov r2, #0x2
-	str r2, [r1, #GPIO_CTRL]
+	orr r2,r2,r1
+	str r2, [r1, #0]
  
 	//load CMU base address
 	ldr r1, =CMU_BASE
 	mov r2, #1				// Load '1' to temp. register r2
 	lsl r2, r2, #CMU_HFPERCLKEN0_GPIO	// Shift '1' to GPIO position
-	str r2, [r1, #CMU_HFPRCLKEN0]		// Write result to control register
+	str r2, [r1, #CMU_HFPERCLKEN0]		// Write result to control register
 
 	// Set up GPIO - PORT_C[0-7] = SWITCHES, PORT_A[8-15] = LEDS
 	// -> SWITCHES: strong pull-up, input (maybe filter) - MODEn = 0b0010, DOUT = 1
 	// -> LEDS: output mode
-	ldr r1,=GPIO_PC_BASE
-	mov r2, #0x22222222 // = 0b0010
-	str r2, [r1, #GPIO_MODEL]
+//	ldr r1,=GPIO_PC_BASE
+//	mov r2, #0x22222222 // = 0b0010
+//	str r2, [r1, #GPIO_MODEL]
 	
-	mov r2, #0xFF
-	str r2, [r1, #GPIO_DOUT]
+//	mov r2, #0xFF
+//	str r2, [r1, #GPIO_DOUT]
 	
 	//LEDS, modeh=0b0100 push pull
 	ldr r1,=GPIO_PA_BASE
-	mov r2, #0x55555555
+	mov r2, #0x55
 	str r2, [r1, #GPIO_MODEH]
 	
 	/////////////////////////////////////////////////////////////////////////////
@@ -119,8 +120,8 @@ gpio_handler:
 //	      b .  // do nothing
 //Set LEDS high
 	ldr r1, =GPIO_PA_BASE
-	mov r2, #0xFF00  //set pin 15-8 high
-	orr r2, [r1, #GPIO_DOUT] 	
+	mov r2, #0xFF  //set pin 15-8 high
+	str r2, [r1, #GPIO_DOUT] 	
 	/////////////////////////////////////////////////////////////////////////////
 	
         .thumb_func
