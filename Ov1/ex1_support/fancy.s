404acac799af101cd6dbd041f59716c6989f99d4
Marjeris Romero <marjeris.romero@gmail.com>
2015-02-07 15:21:29 +0100

    litt opprydding og begynt på fancy

--- Ov1/ex1_support/fancy.s
+++ Ov1/ex1_support/fancy.s
@@ -2,4 +2,6 @@ toggle_this:
 	// TODO: function to toggle the selected LED
 	
 move_dot:
-	// TODO: one dot lit, move left/rigth	
+	// TODO: one dot lit, move left/right
+	// If button 'SW[1|5]' is pressed, move dot left, SW[3|7] move right
+	// If button 'SW[2|6]' is pressed, increase intensity, SW[4|8] decrease (use timers for PWM dimming)
