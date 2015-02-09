e17f971248f4c28ad8747f029d5bb92bc8bcfa12
Torbjørn Viem Ness <tbness@gmail.com>
2015-02-09 02:32:58 +0100

    Levert øving 1

--- Ov1/ex1_support/ex1.s
+++ Ov1/ex1_support/ex1.s
@@ -1,20 +1,6 @@
 .include "efm32gg.s"
 .syntax unified
 .include "fancy.s"
-// Some more register addresses
-	CMU_LFACLKEN0	= 0x058
-	CMU_LFAPRESC0	= 0x068
-
-LETIMER_BASE = 0x40082000
-	LETIMER_CTRL	= 0x000
-	LETIMER_CMD	= 0x004
-	LETIMER_STATUS	= 0x008
-	LETIMER_COMP0	= 0x010
-	LETIMER_COMP1	= 0x014
-	LETIMER_IF	= 0x020
-	LETIMER_IFC	= 0x028
-	LETIMER_IEN	= 0x02C
-	LETIMER_ROUTE	= 0x040
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
@@ -102,11 +88,6 @@ _reset:
 	ldr r4, =GPIO_PA_BASE
 	ldr r5, =GPIO_PC_BASE
 	ldr r6, =GPIO_BASE
-	
-	//Power down SRAM to conserve energy - this didn't work for us, TODO: fjern fra innlevering
-	ldr r0, =EMU_BASE
-	mov r1, #7				// Disable SRAM blocks 1-3
-//	str r1, [r0, #0x4] 			// Write to EMU_MEMCTRL
 
 	//Set high drive strength 
 	mov r2, #0x2
@@ -149,29 +130,13 @@ _reset:
 	str r2, [r1]
 
 	mov r7, #1	// Variable for use in fancy patterns
-	mov r8, #1	// Variable for use in fancy patterns
+	mov r8, #128	// Variable for use in fancy patterns
 
 	//Go to sleep after initializing (see page 100 in Cortex-M3 manual)
 	ldr r1, =SCR
 	mov r2, #6	// Select DEEPSLEEP as the low power mode, and SLEEPONEXIT
 	str r2, [r1]
 	wfi
-
-/*	.thumb_func
-pwm_init:
-	//load CMU base address
-	ldr r1, =CMU_BASE
-	//Set bit #2 high to enable LETIMER0 clock
-	mov r2, #4
-	str r2, [r1, #CMU_LFACLKEN0]
-	// Bits 11-8 in LFAPRESC0 set the prescaler for LETIMER0
-
-	// LETIMER_CTRL: COMP0TOP=1 (COMP0 is top value), UFOA0=3 (PWM for output 0)
-	// PWM: LETn_O0 is set idle on CNT underflow, and active on compare match with COMP1
-
-	// LETIMER_CMD - used for start/stop timer, see page 594 in efm32GG manual
-*/	
-
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
@@ -192,7 +157,7 @@ gpio_handler:
 
 	b move_dot	// Function in fancy.s
 
-// Return point after modifying LED display register
+	// Return here after modifying LED display register
 set_leds:
 	// Left shift because LEDs are on pins 8-15
 	lsl r0, r0, #8
