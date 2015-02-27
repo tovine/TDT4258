3a3b91da118de07db28359b3143839e94529889f
Torbjørn Viem Ness <torbjorn@viem-it.no>
2015-02-27 18:45:41 +0100

    Added header files for everything and organized includes

--- Ov2/ex2-nocs-support/ex2.h
+++ Ov2/ex2-nocs-support/ex2.h
@@ -1,13 +1,13 @@
-#include "efm32gg.h"
 #ifndef EX2_HEADER
 #define EX2_HEADER
 
-#include "gpio.c"
-#include "dac.c"
-#include "timer.c"
+#include "efm32gg.h"
+#include "gpio.h"
+#include "dac.h"
+#include "timer.h"
+#include "interrupt_handlers.h"
 
 /* Declaration of peripheral setup functions */
-void setupDAC();
 void setupNVIC();
 
 
---
+++ Ov2/ex2-nocs-support/timer.h
@@ -0,0 +1,6 @@
+#ifndef TIMER_H_
+#define TIMER_H_
+
+void setupTimer(uint16_t period);
+
+#endif
