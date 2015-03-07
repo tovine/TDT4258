066448c6e9c4eb5c9356cbb768ad9cb75cf2370e
Torbjørn Viem Ness <torbjorn@viem-it.no>
2015-03-07 20:21:32 +0100

    Started writing some energy optimalizations (not yet tested), also estimated the flash memory usage of the sounds + some cleanup and commenting.

--- Ov2/ex2-nocs-support/interrupt_handlers.c
+++ Ov2/ex2-nocs-support/interrupt_handlers.c
@@ -11,31 +11,20 @@
 
 void __attribute__ ((interrupt)) TIMER1_IRQHandler() 
 {  
-  /*
-    TODO feed new samples to the DAC
-    remember to clear the pending interrupt by writing 1 to TIMER1_IFC
-  */  
-/*
-	*DAC0_CH0DATA = sinewave[sample];
-	*DAC0_CH1DATA = sinewave[sample];
-	//else *DAC0_CH1DATA = (squarewave[sample] << 4);
-//	*DAC0_CH0DATA = (rand() % 4095);
-//	*DAC0_CH1DATA = (rand() % 4095);
-	sample++;
+/*	DEBUG: generate white noise to check that the DAC is working correctly
+	*DAC0_CH0DATA = (rand() % 4095);
+	*DAC0_CH1DATA = (rand() % 4095);
 */
-//	if(sample == 0) r = !r;
-//	*GPIO_PA_DOUT = (sample << 8);
 	if(curr_sound == NULL){
 		// Sample pointer NULL/invalid - ouput 0
 		*DAC0_CH0DATA = DAC_IDLE_VAL;
 		*DAC0_CH1DATA = DAC_IDLE_VAL;
-//	} else if (*curr_sample > 0xff) {
 	} else if (curr_sample >= sound_length -1) {
 		// We're at the end of an audio signal - repeat or stop
 		if(!repeat_sound) curr_sound = NULL;
 //		curr_sample = curr_sound;
 		curr_sample = 0;
-		// Clear input interrupt to repeat the sound as long as the button is pressed
+		// Re-set input interrupt to repeat the sound as long as the button is pressed
 		*GPIO_IFS= 0xff;
 	} else {
 		// Send the samples to DAC
@@ -51,26 +40,14 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
 void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler() 
 {
 	handleKeypress();
-    /* TODO handle button pressed event, remember to clear pending interrupt */
 	*GPIO_PA_DOUT = 0xAA00;
 	*GPIO_IFC= 0xff;
-/*	curr_sound = &sinewave[0];
-	if(repeat_sound) repeat_sound = false;
-	else repeat_sound = true;
-	curr_sample = curr_sound;
-*/
 }
 
 /* GPIO odd pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler() 
 {
 	handleKeypress();
-    /* TODO handle button pressed event, remember to clear pending interrupt */
 	*GPIO_PA_DOUT = 0x5500;
 	*GPIO_IFC= 0xff;
-/*	curr_sound = &squarewave[0];
-	if(repeat_sound) repeat_sound = false;
-	else repeat_sound = true;
-	curr_sample = curr_sound;
-*/
 }
--- Ov2/ex2-nocs-support/timer.c
+++ Ov2/ex2-nocs-support/timer.c
@@ -7,20 +7,9 @@
 
 /* function to setup the timer */
 void setupTimer(uint16_t period) {
-  /*
-    TODO enable and set up the timer
-    
-    1. Enable clock to timer by setting bit 6 in CMU_HFPERCLKEN0
-    2. Write the period to register TIMER1_TOP
-    3. Enable timer interrupt generation by writing 1 to TIMER1_IEN
-    4. Start the timer by writing 1 to TIMER1_CMD
-    
-    This will cause a timer interrupt to be generated every (period) cycles. Remember to configure the NVIC as well, otherwise the interrupt handler will not be invoked.
-  */
 	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;
 	*TIMER1_TOP = period;
 	*TIMER1_IEN= 1;
-//	*ISER0 |= (1<<12);
 }
 
 void startTimer(void) {
